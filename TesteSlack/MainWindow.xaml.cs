31036268319514b238dbe03ec7a62f84d804869d
Aluno <Aluno@Digital.net>
2018-04-18 19:16:52 -0300

    second commit

---
+++ TesteSlack/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TesteSlack
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Enviar_Click(object sender, RoutedEventArgs e)
+        {
+            SlackClient client = new SlackClient(link.Text);
+
+            client.PostMessage(text: mensagem.Text, channel: "#general");
+        }
+    }
+}
